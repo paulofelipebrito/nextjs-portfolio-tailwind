654a7c2bad953e37c257a9a277bb032078a8f0c0
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-06-29 22:39:09 -0300

    feat: Navbar component

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div>
       <Head>
-
+        <title>Paulo Felipe | Full Stack Developer</title>
+        <meta name="description" content="I’m a full stack web developer specializing in building exceptional digital experiences." />
+        <link rel="icon" href="/fav.png" />
       </Head>
     </div>
   )
